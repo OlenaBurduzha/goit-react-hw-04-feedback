a01b75dc2ffa46b7443c718ee2de778d889389fc
OlenaBurduzha <maglerb86@gmail.com>
2023-01-09 18:59:20 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import Section from "./Section/Section";
 import Statistics from "./Statistics/Statistics";
 
 function App() {
+  
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
